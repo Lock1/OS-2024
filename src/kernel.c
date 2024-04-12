eb42f5fbef4bb4660b87fedfe32f3845c6f8e534
Lock1 <rizaltanur@outlook.com>
2024-04-13 02:08:26 +0700

    feat: 2.2. User Mode
    - Pretty much smooth sailing with new OS-2024 guide & crt0.s
    - Additionally adjust some makefile "suggestion" folder structure in Book OS-2024

--- src/kernel.c
+++ src/kernel.c
@@ -7,37 +7,39 @@
 #include "header/driver/keyboard.h"
 #include "header/text/framebuffer.h"
 #include "header/filesystem/fat32.h"
+#include "header/memory/paging.h"
 
 void kernel_setup(void) {
     load_gdt(&_gdt_gdtr);
     pic_remap();
     initialize_idt();
     activate_keyboard_interrupt();
-    initialize_filesystem_fat32();
     framebuffer_clear();
     framebuffer_set_cursor(0, 0);
+    initialize_filesystem_fat32();
+    gdt_install_tss();
+    set_tss_register();
 
-    int row = 0, col = 0;
-    keyboard_state_activate();
-    while (true) {
-        char c;
-        get_keyboard_buffer(&c);
-        if (c) {
-            framebuffer_write(row, col, c, 0xF, 0);
-            if (col >= FRAMEBUFFER_WIDTH) {
-                ++row;
-                col = 0;
-            } else {
-                ++col;
-            }
-           framebuffer_set_cursor(row, col);
-        }
-    }
+    // Allocate first 4 MiB virtual memory
+    paging_allocate_user_page_frame(&_paging_kernel_page_directory, (uint8_t*) 0);
+
+    // Write shell into memory
+    struct FAT32DriverRequest request = {
+        .buf                   = (uint8_t*) 0,
+        .name                  = "shell",
+        .ext                   = "\0\0\0",
+        .parent_cluster_number = ROOT_CLUSTER_NUMBER,
+        .buffer_size           = 0x100000,
+    };
+    read(request);
+
+    // Set TSS $esp pointer and jump into shell 
+    set_tss_kernel_current_stack();
+    kernel_execute_user_program((uint8_t*) 0);
 
     while (true);
 }
 
-// TODO: U/K space
 // TODO: Shell
 
 // TODO: Context
